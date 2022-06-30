4163e650b6fc392082e47e9762d9da4ebcb092e9
William Jonathan Mulyadi <91537837+willamjonathan@users.noreply.github.com>
2022-06-30 17:24:04 +0700

    Create 5.cpp

---
+++ number5/5.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+void hashfunction(int mytable[], int tsize, int myarr[], int N)
+{ 
+    int numberofcollision = 0;
+    for (int z = 0; z < N; z++) 
+    {
+        int d = myarr[z] % tsize; 
+
+        if (mytable[d] == -1){ 
+            mytable[d] = myarr[z];
+        }
+
+        else 
+        {
+            numberofcollision ++;
+            cout << "Collide Number " << numberofcollision << endl; 
+
+            for (int x = 0; x < tsize; x++){
+
+                int t = (d + x * x) % tsize; 
+                if (mytable[t] == -1)
+                {
+
+                    mytable[t] = myarr[z]; 
+                    break;
+                }
+            }
+        }
+    }
+
+    for (int z = 0; z < 10; z++)
+    {
+        cout << mytable[z] << " ";
+    }
+}
+ 
+int main()
+{
+    int arr[] = {5, 40, 18, 22, 28, 38, 48, 20, 8};
+
+    int A = 10;
+
+    int hashtable[10];
+ 
+    for (int z = 0; z < 10; z++)
+    {
+        hashtable[z] = -1;
+    }
+    hashfunction(hashtable, 10, arr, A);
+
+    return 0;
+}
